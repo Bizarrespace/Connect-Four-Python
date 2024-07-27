a971f34aee0d2cbf0fcfa8d53d9dd909c901ca5e
Long Vu <78052960+Bizarrespace@users.noreply.github.com>
2024-07-26 17:51:25 -0700

    Messy Game class with printing of board, vertical check

--- connect_n.py
+++ connect_n.py
@@ -1,5 +1,3 @@
-# use enums to represent a piece, either empty, red or yellow for example
-
 from enum import Enum
 
 class piece(Enum):
@@ -23,12 +21,13 @@ class board():
         return self.grid
 
     # Need error handling, if the player inputs a invalid column
-    # Need to return something to indicate if the placement was successful
-    # Need to make a game class that does the placing of the piece instead of having it in board
-        # The game class should also have a run that keeps on asking the user for placement and checks to see if they won.
     def place_piece(self, player_color):
         # Let the user choose which column they want to place a piece at
-        column = int(input("Choose which column to place piece: "))
+        try:
+            column = int(input(f"Choose which column to place piece{player_color.name}: "))
+        except:
+            print("FOR NOW EXIT IF NOT AN INT")
+            return -1
 
         # The piece then has to go to the lowest row possible, or stop when they hit another piece, their own piece, or the bottom
         for row in range(self.row - 1, -1, -1):
@@ -38,9 +37,71 @@ class board():
                 self.grid[row][column] = player_color
                 break
 
-        print(self.grid)
+        return (row, column)
+    
+    def print_board(self):
+        return_board = ""
+
+        for r in range(len(self.grid)):
+            for c in self.grid[r]:
+                if c == piece.RED:
+                    return_board += "R "
+                elif c == piece.YELLOW:
+                    return_board += "Y "
+                elif c == piece.EMPTY:
+                    return_board += "0 "
+            return_board += "\n"
+
+        print(return_board)
         
 
+class Game():
+    def __init__(self, row, col, connect_n):
+        self.game = board(row, col, connect_n)
+        self.connect_n = connect_n
+        self.player = 1
+        self.run()
 
-game = board(6, 6, 4)
-game.place_piece(piece.RED)
+    def run(self):
+        game_running = True 
+        while game_running:
+            if self.player == 1:
+                place_tuple = self.game.place_piece(piece.RED)
+                win = self.check_win(*place_tuple, piece.RED)
+                if win == True:
+                    print(f"Player Red has won!")
+                    self.game.print_board()
+                    break
+            elif self.player == 2:
+                place_tuple = self.game.place_piece(piece.YELLOW)
+                win = self.check_win(*place_tuple, piece.YELLOW)
+                if win == True:
+                    print(f"Player Red has Yellow!")
+                    self.game.print_board()
+                    break
+
+            if place_tuple == -1:
+                game_running = False
+            else:
+                self.game.print_board()
+                if self.player == 1:
+                    self.player = 2
+                else:
+                    self.player = 1
+
+    def check_win(self, r, c, player):
+        grid = self.game.get_grid()
+        
+
+        # Check for vertical 
+        pieces_in_a_row = 0
+        for r in range(len(grid)):
+            if grid[r][c] == player:
+                pieces_in_a_row += 1
+
+                if pieces_in_a_row == self.connect_n:
+                    return True
+            else: 
+                pieces_in_a_row = 0
+
+game = Game(6, 6, 4)
